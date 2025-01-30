17a2837e84761b76a46789570703d183235ecb12
Lucy Jiménez <lucy.jimenez.mejia@gmail.com>
2025-01-29 22:55:43 -0500

    Mejoras en componente footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,12 @@
-import './Footer.css';
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="footer container">
-      <p>Lucy Jmenez. Todos los derechos reservados. 2025</p>
+      <p>
+        Lucy Jmenez. Todos los derechos reservados. &copy;{" "}
+        {new Date().getFullYear()}
+      </p>
     </div>
   );
 };
