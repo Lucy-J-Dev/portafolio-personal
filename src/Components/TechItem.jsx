3267a9b07b43711526b031c4d9d1b57eef0f38ff
Lucy Jiménez <lucy.jimenez.mejia@gmail.com>
2025-01-29 22:57:22 -0500

    Creación del componente TechItem y diseño responsive

--- src/Components/TechItem.jsx
+++ src/Components/TechItem.jsx
@@ -1,5 +1,18 @@
+import { FaHtml5 } from "react-icons/fa";
+import "./TechItem.css";
+
 const TechItem = () => {
-  return <div>TechItem</div>;
+  return (
+    <div className="tech-item-container">
+      <div className="tech-item-icon">
+        <FaHtml5 />
+      </div>
+      <div className="tech-item-description">
+        <h3>HTML</h3>
+        <p>Estructura</p>
+      </div>
+    </div>
+  );
 };
 
 export default TechItem;
