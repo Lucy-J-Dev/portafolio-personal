eb0b77a02120c5e8ecdb105977add77a3e77684f
Lucy Jiménez <lucy.jimenez.mejia@gmail.com>
2025-01-30 22:17:25 -0500

    Arreglar estilo de navbar y diseño general, colocar iconos de menú en tamaño de celular

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Cosas por hacer
 
 [] Diseño responsive para navbar
-[] Cuando se hace scroll el navbar debería cambiar de color para evitar errores de visualizacion
+[x] Cuando se hace scroll el navbar debería cambiar de color para evitar errores de visualizacion
 [] Modo dark/light
 [x] Diseño responsive para sección Technologies
 [x] Diseño responsive para sección About
@@ -14,15 +14,18 @@
 [] Aplicar SEO en la pagina
 [] Componentes para tecnologia TechItem
 [] Componentes para proyecto ProjectItem
-[x] Agregar links a anchr de redes sociales
+[] Agregar links a anchr de redes sociales
 [x] En boton enviar busar icono de avion
 [x] Buscar icono alternativo para Github que sea parecido al de Linkedin
 [] Hacer el h1 un elemento escondido con el nombre
 [] Modificar el Titulo principal de la pagina
 [] Crear el CV y hace que al dar clik se descargue
+[] Boton para regresar al inicio de la pantalla
 
 # Tareas
 
 1. Incluir imagen de aspirante aunque sea un avatar
 2. Hacer diseño responsive del Hero (que las redes sociales salgan verticales, la descripción y los botónes se ubiquen en el lado izquierdo de la pagina y que la foto salga del lado derecho)
 3. Usar la data de la lista de las tecnologías para crear los elementos TechItem en la sección de Technologies (pasar la data del componente padre al componente hijo como se vio en el tutorial de introducción a React)
+4. Revisar y modificar los tamaños de fuente
+5. Actualizar el icono que maneja el mode light/dark tal como se hace con el icono del menu
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,54 +1,82 @@
+import { useState } from "react";
 import "./Navbar.css";
 import { FaRegSun } from "react-icons/fa";
 import { FaRegMoon } from "react-icons/fa";
+import { RiCloseLargeLine, RiMenu4Fill } from "react-icons/ri";
 
 const Navbar = () => {
   const links = [
     {
+      id: 1,
       ref: "#about",
       text: "Sobre mi",
+      active: true,
     },
     {
+      id: 2,
       ref: "#projects",
       text: "Proyectos",
+      active: false,
     },
     {
+      id: 3,
       ref: "#contact",
       text: "Contacto",
+      active: false,
     },
   ];
 
+  const [showMenu, setShowMenu] = useState(true);
+  const [ligthMode, setLigthMode] = useState(true);
+
   return (
-    <header>
-      <div className="container">
-        <nav>
-          <div className="logo">
-            <a href="#">
-              <span>Lucy </span>Jimenez
-            </a>
+    <header className="navbar">
+      <nav className="container">
+        {/* La marca personal */}
+        <div className="navbar-brand">
+          <h1>
+            <span>Lucy </span>Jimenez
+          </h1>
+        </div>
+        {/* Menu con los items de la pagina */}
+        <div className="navbar-menu">
+          <ul>
+            {links.map((link, index) => {
+              return (
+                <li key={index}>
+                  <a href={link.ref}>{link.text}</a>
+                </li>
+              );
+            })}
+          </ul>
+          {/* Botones para dark/light mode */}
+          <div className="navbar-menu-mode">
+            <FaRegSun />
+            <FaRegMoon />
           </div>
-          <div className="list-darkmode-menu">
-            <ul>
-              {links.map((link, index) => {
-                return (
-                  <li key={index}>
-                    <a href={link.ref}>{link.text}</a>
-                  </li>
-                );
-              })}
-            </ul>
-            <label className="mode">
-              {/* <input type='checkbox' checked='checked' id='darkModeButton' /> */}
-              <i className="fa-solid fa-sun">
-                <FaRegSun />
-              </i>
-              <i className="fa-solid fa-moon">
-                <FaRegMoon />
-              </i>
-            </label>
-          </div>
-        </nav>
-      </div>
+        </div>
+        {/* Botones del menu */}
+        <div className="navbar-menu-buttons">
+          {/* mostrar menu es verdadero entonces que aparezca la x */}
+          {/* mostrar menu es falso entonces que aparezca el icono de las lineas */}
+          {showMenu ? (
+            <RiCloseLargeLine
+              onClick={() => {
+                setShowMenu((prevState) => !prevState);
+              }}
+            />
+          ) : (
+            <RiMenu4Fill
+              onClick={() => {
+                setShowMenu((prevState) => !prevState);
+              }}
+            />
+          )}
+
+          {/* <RiMenu4Fill />
+          <RiCloseLargeLine /> */}
+        </div>
+      </nav>
     </header>
   );
 };
