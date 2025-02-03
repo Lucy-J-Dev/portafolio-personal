9e54a5a5b9404f7f8dd492e4c21814d248448fba
Lucy Jimenez <lucy.jimenez.mejia@gmail.com>
2025-02-03 17:48:25 -0500

    Arreglo del componente technologies

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Technologies from "./components/Technologies";
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import Technologies from './components/Technologies';
 
 function App() {
   return (
