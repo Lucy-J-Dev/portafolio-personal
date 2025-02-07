8cf73963954e0483cc8cf6d9568ab5a236687b92
Lucy Jimenez <lucy.jimenez.mejia@gmail.com>
2025-02-06 20:44:47 -0500

    Arreglo en el componente ScrollUpButton,  Verificación de tareas en el Readme, Cambio de logos en el componente Technologies

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
 # Cosas por hacer
 
-[] Diseño responsive para navbar
+[x] Diseño responsive para navbar
 [x] Cuando se hace scroll el navbar debería cambiar de color para evitar errores de visualizacion
-[] Modo dark/light
+[x] Modo dark/light
 [x] Diseño responsive para sección Technologies
 [x] Diseño responsive para sección About
 [x] Diseño responsive para sección Proyects
 [x] Diseño responsive para sección Contact
-[] Formulario en Contact
+[x] Formulario en Contact
 [] Foto de aspirante
 [x] Footer
 [x] Traducir sección about de inglés a español
 [] Aplicar SEO en la pagina
-[] Componentes para tecnologia TechItem
+[x] Componentes para tecnologia TechItem
 [] Componentes para proyecto ProjectItem
 [] Agregar links a anchr de redes sociales
 [x] En boton enviar busar icono de avion
 [x] Buscar icono alternativo para Github que sea parecido al de Linkedin
-[] Hacer el h1 un elemento escondido con el nombre
-[] Modificar el Titulo principal de la pagina
+[x] Hacer el h1 un elemento escondido con el nombre
+[x] Modificar el Titulo principal de la pagina
 [] Crear el CV y hace que al dar clik se descargue
-[] Boton para regresar al inicio de la pantalla
+[x] Boton para regresar al inicio de la pantalla
 
 # Tareas
 
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,26 +1,54 @@
-import TechItem from "./TechItem";
-import { FaHtml5 } from "react-icons/fa";
-import "./Technologies.css";
+import TechItem from './TechItem';
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaGithubSquare,
+  FaReact,
+  FaBootstrap,
+} from 'react-icons/fa';
+import { IoLogoJavascript } from 'react-icons/io5';
+import { DiVisualstudio } from 'react-icons/di';
+import { MdOutlineTerminal } from 'react-icons/md';
+import { PiFigmaLogoFill } from 'react-icons/pi';
+import { SiJira, SiTailwindcss } from 'react-icons/si';
+
+import './Technologies.css';
 
 // Datos de ejemplo
 
 const technologies = [
-  { icon: <FaHtml5 />, title: "HTML", description: "Estructura" },
-  { icon: <FaHtml5 />, title: "CSS", description: "Estilo" },
-  { icon: <FaHtml5 />, title: "JavaScript", description: "Interacción" },
-  { icon: <FaHtml5 />, title: "GitHub", description: "Versionamiento" },
+  { icon: <FaHtml5 />, title: 'HTML', description: 'Estructura' },
+  { icon: <FaCss3Alt />, title: 'CSS', description: 'Estilo' },
   {
-    icon: <FaHtml5 />,
-    title: "Visual Studio Code",
-    description: "Editor de código",
+    icon: <IoLogoJavascript />,
+    title: 'JavaScript',
+    description: 'Interacción',
   },
-  { icon: <FaHtml5 />, title: "Terminal", description: "Línea de comandos" },
-  { icon: <FaHtml5 />, title: "Figma", description: "Diseño" },
-  { icon: <FaHtml5 />, title: "Jira", description: "Gestión" },
-  { icon: <FaHtml5 />, title: "React", description: "Framework" },
-  { icon: <FaHtml5 />, title: "Bootstrap", description: "Frontend toolkit" },
-  { icon: <FaHtml5 />, title: "Tailwind CSS", description: "CSS Framework" },
-  { icon: <FaHtml5 />, title: "??", description: "???" },
+  { icon: <FaGithubSquare />, title: 'GitHub', description: 'Versionamiento' },
+  {
+    icon: <DiVisualstudio />,
+    title: 'Visual Studio Code',
+    description: 'Editor de código',
+  },
+  {
+    icon: <MdOutlineTerminal />,
+    title: 'Terminal',
+    description: 'Línea de comandos',
+  },
+  { icon: <PiFigmaLogoFill />, title: 'Figma', description: 'Diseño' },
+  { icon: <SiJira />, title: 'Jira', description: 'Gestión' },
+  { icon: <FaReact />, title: 'React', description: 'Framework' },
+  {
+    icon: <FaBootstrap />,
+    title: 'Bootstrap',
+    description: 'Frontend toolkit',
+  },
+  {
+    icon: <SiTailwindcss />,
+    title: 'Tailwind CSS',
+    description: 'CSS Framework',
+  },
+  { icon: <FaHtml5 />, title: '??', description: '???' },
 ];
 
 const Technologies = () => {
