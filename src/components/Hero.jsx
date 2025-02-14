e526c35fe6b72e3f99a79f1b5f25bdce1bdf7277
Lucy Jiménez <lucy.jimenez.mejia@gmail.com>
2025-02-13 20:20:00 -0500

    Mejorar hero y diseño responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,43 +1,54 @@
-import './Hero.css';
-import { MdLocalPhone, MdOutlineFileDownload } from 'react-icons/md';
-import SocialMedia from './SocialMedia';
-import HeroImage from '../assets/Foto-Lucy-Jimenez.jpeg';
+import "./Hero.css";
+import { MdLocalPhone, MdOutlineFileDownload } from "react-icons/md";
+import { FaSquareGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import HeroImage from "../assets/Foto-Lucy-Jimenez.jpeg";
 
 const Hero = () => {
   return (
     <>
       <div className="hero container">
-        <div>
-          {/* Texto en el hero */}
-          <div className="hero-container">
-            <p className="hero-subtitle">Hola...</p>
-            <p className="hero-title">
-              Soy <span>Lucy Jiménez </span>desarrolladora Frontend
-            </p>
-            <p>
-              Bienvenidos a mi portafolio como desarrolladora Frontend. Soy
-              Lucy, una programadora que está iniciando esta nueva etapa con una
-              fuerte pasión por el diseño y la creación de páginas web
-              llamativas, adaptables y amigables con el usuario.
-            </p>
-            <div className="container-button">
-              <button className="btn primary">
-                <span>Contactame</span>
-                <MdLocalPhone />
-              </button>
+        <div className="hero-container">
+          <div className="hero-right">
+            {/* Texto en el hero */}
+            <div className="hero-information">
+              <p className="hero-subtitle">Hola...</p>
+              <p className="hero-title">
+                Soy <span>Lucy Jiménez </span>desarrolladora Frontend
+              </p>
+              <p>
+                Bienvenidos a mi portafolio como desarrolladora Frontend. Soy
+                Lucy, una programadora que está iniciando esta nueva etapa con
+                una fuerte pasión por el diseño y la creación de páginas web
+                llamativas, adaptables y amigables con el usuario.
+              </p>
+              <div className="container-button">
+                <button className="btn primary">
+                  <span>Contactame</span>
+                  <MdLocalPhone />
+                </button>
 
-              <button className=" btn secondary">
-                <span>Descargar CV</span>
-                <MdOutlineFileDownload />
-              </button>
+                <button className=" btn secondary">
+                  <span>Descargar CV</span>
+                  <MdOutlineFileDownload />
+                </button>
+              </div>
+            </div>
+
+            {/* Redes sociales */}
+            <div className="hero-social-media">
+              <a href="">
+                <FaSquareGithub />
+              </a>
+              <a href="">
+                <FaLinkedin />
+                {/* <TbBrandLinkedinFilled /> */}
+              </a>
             </div>
           </div>
 
-          {/* Redes sociales */}
-          <SocialMedia />
-
           {/* Imagen personal */}
-          <div className="container-photo">
+          <div className="hero-left">
             <img
               src={HeroImage}
               alt="Foto de presentación de perfil de Lucy Jiménez"
